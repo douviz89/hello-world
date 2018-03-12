5041ac1cc23a2863ffbf20aedd01402b83eb3809
douviz <douviz@192.168.1.20>
2018-03-12 02:11:08 +0100

    test commit from home

--- testGIT/src/testGIT/TestMain.java
+++ testGIT/src/testGIT/TestMain.java
@@ -4,7 +4,7 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world ! edited");
+		System.out.println("hello world ! edited from home");
 		
 	}
 
